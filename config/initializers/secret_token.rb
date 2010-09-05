ac0875252f317aa19c8566afd302e4b9b456ba26
Marek Goldmann <marek.goldmann@gmail.com>
2010-09-05 09:00:29 +0200

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wspolnota::Application.config.secret_token = 'd9af88a11192dda3099c3b6456b637adef96cdb5467e3e28fe5250f299a81825422516f1fa2e4ab62b3689344a1659a0dbcd5e305850e1af2756f8ae03c50a48'
